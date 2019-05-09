a32508562707edc0718730343442f482cadaa410
Farbod Dadashian <40148082+Farbod1876@users.noreply.github.com>
2019-05-09 12:57:36 +0200

    Add files via upload

---
+++ src/components/Titles.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+class Titles extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h1 className="title-container-title">Weather Finder</h1>
+        <h3 className="weather-container-subtitle">
+          Temperature, conditions and more...
+        </h3>
+      </div>
+    );
+  }
+}
+
+export default Titles;
